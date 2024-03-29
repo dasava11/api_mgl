b7ba1138af36cc7ae891e970149cc5cdbf565a31
dasava11 <dasava11@outlook.com>
2024-03-29 18:36:35 -0500

    change_dataType phone

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -14,7 +14,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       phone: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: true,
       },
       email: {
